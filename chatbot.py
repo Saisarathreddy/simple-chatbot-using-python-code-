7c423faf1036607dea0382cf4002f55a791d8544
Saisarathreddy <138870135+Saisarathreddy@users.noreply.github.com>
2023-07-09 10:11:41 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,37 @@
+import random
+
+
+responses = {
+    "hello": "Hello, how can I help you?",
+    "how are you": "I'm good, thank you!",
+    "what's your name?": "My name is ChatBot.",
+    "bye": "Goodbye! Have a great day!",
+    "how is the day":"great ! what about you",
+    "how is the weather now":"it cool"
+    
+    
+}
+
+def generate_response(user_input):
+    user_input = user_input.lower()
+    
+    
+    if user_input in responses:
+        return responses[user_input]
+    else:
+        return "I'm sorry, I don't understand."
+
+
+while True:
+    
+    user_input = input("User: ")
+    
+    
+    response = generate_response(user_input)
+    
+    
+    print("ChatBot:", response)
+    
+
+    if user_input.lower() == "how is the weather now":
+        break
